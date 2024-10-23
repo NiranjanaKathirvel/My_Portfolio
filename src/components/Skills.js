2bd33a16de3d240469deedc710230bcd89ae5371
Niranjana Kathirvel <84134386+NiranjanaKathirvel@users.noreply.github.com>
2024-10-23 18:08:31 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -45,6 +45,9 @@ const Skills = () => {
                 <div className="skill">
                     <img className="skill-logo" src={web} alt="Your Name" style={{ width: '100%', height: '100%' }} />
                 </div>
+                <div className="skill">
+                    <img className="skill-logo" src={figma} alt="Your Name" style={{ width: '100%', height: '100%' }} />
+                </div>
             </div>
         </section>
     );
